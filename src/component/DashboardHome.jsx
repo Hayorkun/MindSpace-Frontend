3b82428876b23a0cbfcd211106d89c283629c6ef
Ayomide Yerokun <Ayomideyerokun54@gmail.com>
2026-08-05 16:11:51 +0100

    fix: mobile screen  overflow

--- src/component/DashboardHome.jsx
+++ src/component/DashboardHome.jsx
@@ -1,55 +1,15 @@
-import { Plus, XIcon } from "lucide-react";
+import { Plus, } from "lucide-react";
 import { useTask } from "../context/TaskContext";
-import { useState } from "react";
 import { SlCalender, SlCheck } from "react-icons/sl";
 import { CgDanger } from "react-icons/cg";
 import { GiSandsOfTime } from "react-icons/gi";
 import TaskCard from "./TaskCard";
+import CreateTaskPanel from "./CreateTaskPanel";
 
 const DashboardHome = () => {
-  const { openCreatePanel, isPanelOpen, closePanel, addTask, tasks } =
+  const { openCreatePanel, tasks } =
     useTask();
 
-  const initialState = {
-    title: "",
-    description: "",
-    dueDate: "",
-    priority: "low",
-    category: "personal",
-    status: "pending",
-  };
-
-  const [taskData, setTaskData] = useState(initialState);
-  const [submitting, setSubmitting] = useState(false);
-
-  const handleCancel = () => {
-    setTaskData(initialState);
-    closePanel();
-  };
-
-  const handleChange = (f) => {
-    setTaskData((prev) => ({
-      ...prev,
-      [f.target.name]: f.target.value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    console.log(taskData);
-
-    try {
-      setSubmitting(true);
-      await addTask(taskData);
-      setTaskData(initialState);
-      closePanel();
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
 
   const Stats = [
     {
@@ -152,151 +112,7 @@ const DashboardHome = () => {
         </div>
         {/* MOBILE VIEW */}
       </div>
-      {isPanelOpen && (
-        <>
-          <div
-            className="fixed inset-0 bg-black/50 backdrop-blur-xs z-40"
-            onClick={closePanel}
-          />
-
-          <div className="fixed inset-0 flex flex-col items-center justify-center z-50">
-            <div
-              className="w-90 lg:w-150 rounded-xl bg-gray-300/80 dark:bg-gray-900 py-6 shadow-2xl"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <div className="flex px-6 justify-between items-start mb-5 lg:mb-7 border-b border-gray-400 dark:border-gray-700">
-                <div>
-                  <h2 className="text-gray-700 dark:text-gray-400 font-heading font-bold text-2xl lg:text-3xl leading-tight mb-1">
-                    Create New Task
-                  </h2>
-                  <p className="font-body text-sm lg:text-md text-gray-700 dark:text-gray-400 leading-relaxed lg:tracking-wider">
-                    Add the details of your ext high-performance objective
-                  </p>
-                </div>
-                <button onClick={closePanel} className="cursor-pointer">
-                  <XIcon />
-                </button>
-              </div>
-              <form onSubmit={handleSubmit}>
-                <div className="px-6 space-y-5 mb-5">
-                  <label
-                    htmlFor="title"
-                    className="flex text-gray-600 dark:text-gray-400 flex-col gap-3 text-md font-medium font-body leading-relaxed"
-                  >
-                    Task Name
-                    <input
-                      type="text"
-                      name="title"
-                      value={taskData.title}
-                      onChange={handleChange}
-                      className="border dark:border-gray-600 border-gray-500 bg-gray-400/50 dark:bg-gray-700 rounded-md px-1.5 py-1 text-sm lg:h-10 lg:text-md text-black dark:text-white"
-                      placeholder="e.g. Q1 financial audit"
-                    />
-                  </label>
-                  <label
-                    htmlFor="description"
-                    className="flex text-gray-600 dark:text-gray-400 flex-col gap-3 text-md font-medium font-body leading-relaxed"
-                  >
-                    Description
-                    <textarea
-                      name="description"
-                      value={taskData.description}
-                      onChange={handleChange}
-                      placeholder="Briefly outline the core objective"
-                      className="text-black dark:text-white border p-2 dark:border-gray-600 border-gray-500 bg-gray-400/50 dark:bg-gray-700 rounded-md  text-sm h-17 lg:h-25 lg:text-md"
-                    />
-                  </label>
-                  <div className="flex justify-between gap-5 lg:gap-10">
-                    <label
-                      htmlFor="dueDate"
-                      className="flex flex-1 flex-col text-gray-600 dark:text-gray-400 gap-2 text-md font-medium font-body leading-relaxed"
-                    >
-                      Due Date
-                      <input
-                        name="dueDate"
-                        value={taskData.dueDate}
-                        onChange={handleChange}
-                        type="date"
-                        className="border dark:border-gray-600 border-gray-500 bg-gray-400/50 dark:bg-gray-700 rounded-md h-8 lg:h-10 text-black dark:text-white"
-                      />
-                    </label>
-                    <label
-                      htmlFor="priority"
-                      className="flex flex-1 flex-col text-gray-600 dark:text-gray-400 gap-2 text-md font-medium font-body leading-relaxed"
-                    >
-                      Priority
-                      <select
-                        id="priority"
-                        name="priority"
-                        value={taskData.priority}
-                        onChange={handleChange}
-                        className="border dark:border-gray-600 border-gray-500 bg-gray-400/50 dark:bg-gray-700 rounded-md h-8 lg:h-10 text-black dark:text-white"
-                      >
-                        <option value="low">Low</option>
-                        <option value="medium">Medium</option>
-                        <option value="high">High</option>
-                      </select>
-                    </label>
-                  </div>
-                  <div className="flex gap-5">
-                    <label
-                      htmlFor="status"
-                      className="flex flex-1 flex-col text-gray-600 dark:text-gray-400 gap-2 text-md font-medium font-body leading-relaxed"
-                    >
-                      Status
-                      <select
-                        id="status"
-                        name="status"
-                        value={taskData.status}
-                        onChange={handleChange}
-                        className="border dark:border-gray-600 border-gray-500 bg-gray-400/50 dark:bg-gray-700 rounded-md h-8 lg:h-10 text-black dark:text-white"
-                      >
-                        <option value="pending">Pending</option>
-                        <option value="in-progress">In Progress</option>
-                        <option value="completed">Completed</option>
-                      </select>
-                    </label>
-                    <label
-                      htmlFor="category"
-                      className="flex flex-1 flex-col text-gray-600 dark:text-gray-400 gap-2 text-md font-medium font-body leading-relaxed"
-                    >
-                      Category
-                      <select
-                        id="category"
-                        name="category"
-                        value={taskData.category}
-                        onChange={handleChange}
-                        className="border dark:border-gray-600 border-gray-500 bg-gray-400/50 dark:bg-gray-700 rounded-md h-8 lg:h-10 text-black dark:text-white"
-                      >
-                        <option value="personal">Personal</option>
-                        <option value="finance">Finance</option>
-                        <option value="health">Health</option>
-                        <option value="work">Work</option>
-                      </select>
-                    </label>
-                  </div>
-                </div>
-                <div className="flex justify-between border-t items-center px-6 py-2 border-gray-400 dark:border-gray-700">
-                  <button
-                    className="font-body text-gray-600 dark:text-gray-300 cursor-pointer"
-                    type="reset"
-                    onClick={handleCancel}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    disabled={submitting}
-                    type="submit"
-                    className="px-3 py-1.5 bg-indigo-600 text-white text-sm font-normal font-body rounded-md cursor-pointer"
-                  >
-                    Create Task
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </>
-      )}
+      <CreateTaskPanel/>
       <button
         onClick={openCreatePanel}
         className="fixed bottom-6 right-6 flex gap-1 items-center justify-center bg-indigo-500 px-3 py-2 rounded-full text-gray-200 shadow-xl hover:bg-indigo-600 hover:scale-105 transition cursor-pointer"
--- src/component/Tasks.jsx
+++ src/component/Tasks.jsx
@@ -1,9 +1,15 @@
 import { useTask } from "../context/TaskContext";
 import { Edit, Trash2, Calendar, Plus } from "lucide-react";
-
+import CreateTaskPanel from "./CreateTaskPanel";
 
 const Tasks = () => {
-  const { filteredTasks, isLoading, removeTask, openEditPanel, openCreatePanel } = useTask();
+  const {
+    filteredTasks,
+    isLoading,
+    removeTask,
+    openEditPanel,
+    openCreatePanel,
+  } = useTask();
 
   if (isLoading) {
     return (
@@ -16,17 +22,17 @@ const Tasks = () => {
   if (filteredTasks.length === 0) {
     return (
       <>
-      <div className="flex flex-col items-center justify-center h-40 gap-2">
-        <p className="text-gray-500 dark:text-gray-400">No tasks yet</p>
-        <p className="text-sm text-gray-400">
-          Create your first task to get started
-        </p>
-      </div>
+        <div className="flex flex-col items-center justify-center h-40 gap-2">
+          <p className="text-gray-500 dark:text-gray-400">No tasks yet</p>
+          <p className="text-sm text-gray-400">
+            Create your first task to get started
+          </p>
+        </div>
 
-       <button className="fixed bottom-6 right-6 flex gap-1 items-center justify-center bg-indigo-500 px-3 py-2 rounded-full text-gray-200 shadow-lg hover:bg-indigo-600 hover:scale-105 transition">
-        <Plus />
-        <span className="hidden lg:flex font-body font-medium">New Task</span>
-      </button>
+        <button className="fixed bottom-6 right-6 flex gap-1 items-center justify-center bg-indigo-500 px-3 py-2 rounded-full text-gray-200 shadow-lg hover:bg-indigo-600 hover:scale-105 transition">
+          <Plus />
+          <span className="hidden lg:flex font-body font-medium">New Task</span>
+        </button>
       </>
     );
   }
@@ -38,53 +44,69 @@ const Tasks = () => {
   };
 
   return (
-    <section className="flex relative p-5">
-      <div className="flex flex-col gap-3 flex-1">
+    <section className="p-5">
+      <div className="flex flex-col gap-3 lg:flex-1">
         {filteredTasks.map((task) => (
           <div
             key={task._id}
-            className="flex items-center gap-3 border border-gray-200 dark:border-gray-700 rounded-lg px-4 py-3"
+            className="flex lg:flex-row justify-between border border-gray-600 dark:border-gray-700 rounded-lg px-4 py-3"
           >
-            <div
-              className={`w-2 h-2 rounded-full ${statusColor[task.status]}`}
-            />
+            <div className="flex items-center gap-2">
+              <div
+                className={`w-2 h-2 rounded-full ${statusColor[task.status]}`}
+              />
 
-            <div className="flex-1 min-w-0">
-              <p className="font-medium truncate">{task.title}</p>
-              <p className="text-sm text-gray-500 truncate w-70">
-                {task.description}
-              </p>
+              <div>
+                <div className="flex-1 min-w-0">
+                  <p className="font-medium truncate">{task.title}</p>
+                  <p className="text-sm text-gray-500 truncate w-50">
+                    {task.description}
+                  </p>
+                </div>
+              </div>
             </div>
+            <div className="flex flex-col">
+              <div>
+                <span className="text-xs px-2 py-1 rounded-full bg-indigo-100 text-indigo-700">
+                  {task.category}
+                </span>
 
-           <div className="flex ">
-             <span className="text-xs px-2 py-1 rounded-full bg-indigo-100 text-indigo-700">
-              {task.category}
-            </span>
+                <span className="text-xs px-2 py-1 rounded-full bg-orange-100 text-orange-700">
+                  {task.priority}
+                </span>
+              </div>
 
-            <span className="text-xs px-2 py-1 rounded-full bg-orange-100 text-orange-700">
-              {task.priority}
-            </span>
+              <div>
+                <span className="text-xs text-gray-400 flex items-center gap-1">
+                  <Calendar size={14} />
+                  {task.dueDate
+                    ? new Date(task.dueDate).toLocaleDateString()
+                    : "No date"}
+                </span>
 
-            <span className="text-xs text-gray-400 flex items-center gap-1">
-              <Calendar size={14} />
-              {task.dueDate
-                ? new Date(task.dueDate).toLocaleDateString()
-                : "No date"}
-            </span>
+                <button onClick={() => openEditPanel(task)}>
+                  <Edit
+                    size={16}
+                    className="text-gray-500 hover:text-indigo-600"
+                  />
+                </button>
 
-            <button onClick={() => openEditPanel(task)}>
-              <Edit size={16} className="text-gray-500 hover:text-indigo-600" />
-            </button>
-
-            <button onClick={() => removeTask(task._id)}>
-              <Trash2 size={16} className="text-gray-500 hover:text-red-600" />
-            </button>
-           </div>
+                <button onClick={() => removeTask(task._id)}>
+                  <Trash2
+                    size={16}
+                    className="text-gray-500 hover:text-red-600"
+                  />
+                </button>
+              </div>
+            </div>
           </div>
         ))}
-       
       </div>
-       <button onClick={openCreatePanel} className="fixed bottom-6 right-6 flex gap-1 items-center justify-center bg-indigo-500 px-3 py-2 rounded-full text-gray-200 shadow-lg hover:bg-indigo-600 hover:scale-105 transition">
+      <CreateTaskPanel />
+      <button
+        onClick={openCreatePanel}
+        className="fixed bottom-6 right-6 flex gap-1 items-center justify-center bg-indigo-500 px-3 py-2 rounded-full text-gray-200 shadow-lg hover:bg-indigo-600 hover:scale-105 transition"
+      >
         <Plus />
         <span className="hidden lg:flex font-body font-medium">New Task</span>
       </button>
